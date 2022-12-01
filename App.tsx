fd3c2c5321f40af625653f6936ea18480609585c
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-12-01 17:34:33 +0900

    fix: install ts dependencies and add jest.config.js file

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Binder App Home Page</Text>
+      <Text>Binder App Home</Text>
       <StatusBar style="auto" />
     </View>
   );
