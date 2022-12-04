7df7053e1898931f69f960d5ba78e3a6eaac2741
NicoletaBararu <nicoletabararu12@gmail.com>
2022-12-04 11:34:49 +0900

    feat: create development build

---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "../components/Themed";
+
+
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>
+                Login 
+            </Text>
+        </View>
+    )
+}
