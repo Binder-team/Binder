2eda7868517318bb79028ef0aa5d6f757830507b
NicoletaBararu <nicoletabararu12@gmail.com>
2022-12-04 23:47:21 +0900

    feat: create buttons My books and Liked Books

--- screens/MyPageScreen.tsx
+++ screens/MyPageScreen.tsx
@@ -1,18 +1,33 @@
-import { StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { white } from 'react-native-paper/lib/typescript/styles/colors';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 // import { RootTabScreenProps } from '../types';
 
+
+
+
 export default function MyPageScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>My Page Screen</Text>
+      <Text style={styles.title}>Profile</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <View >
+          <TouchableOpacity style={styles.button}>
+          <Text>My Books</Text>
+          </TouchableOpacity>
+        </View>
+        <View >
+          <TouchableOpacity style={styles.button}>
+            <Text>Liked Books</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/AddBooksScreen.tsx" />
     </View>
   )
-  
-
 }
 
 const styles = StyleSheet.create({
@@ -24,6 +39,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  button: {
+    flex: 1,
+    width: '100%', 
+    height: 30,
+    backgroundColor: '#008B8B',
+    padding: 10,
   },
   separator: {
     marginVertical: 30,
