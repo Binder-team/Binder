1fd36d1b2153625102230510d23ebd1ee8a5e981
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-12-05 21:45:24 +0900

    add submit button to AddBooksScreen

--- components/AddBook.tsx
+++ components/AddBook.tsx
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
-import { Text, TextInput, StyleSheet, View } from "react-native";
+import { TouchableOpacity, Text, TextInput, StyleSheet, View, Button} from "react-native";
 import axios from 'axios'
 const AddBooks = () => {
   
-  
+    axios.post('https://binderapp-server.herokuapp.com/api/user_books', {
+        isbn: 'isbn',
+        condition:'int'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     return (
         <View>
-          <Text>ISBN:</Text>
+          <Text>ISBN:.....</Text>
           <TextInput placeholder='enter ISBN'/>
           <Text>Book Title:</Text>
           <TextInput placeholder='enter book title'/>
           <Text>Condition</Text>
           <TextInput placeholder='enter a number from 1 - 10'/>
+          <View>
+            <TouchableOpacity>
+            <Button title='SUBMIT'/>
+          </TouchableOpacity>
+          </View>
+          
         </View>
   );
 };
