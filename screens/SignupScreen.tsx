316a6f8889ee0fb8a6d248e875dbf4f4e8e86ba6
NicoletaBararu <nicoletabararu12@gmail.com>
2022-12-07 02:37:46 +0900

    fix: fix Signup button

--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import FormButton from '../components/FormButton';
 import FormInput from '../components/FormInput';
+import { AuthContext } from '../navigation/AuthProvider';
 
 
 
- export default function SignupScreen() {
+ export default function SignupScreen({navigation}) {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { register } = useContext(AuthContext);
   
   return (
     <View style={styles.container}>
@@ -27,7 +29,7 @@ import FormInput from '../components/FormInput';
         onChangeText={userPassword => setPassword(userPassword)}
         secureTextEntry={true}
       />
-      <FormButton buttonTitle='Signup' onPress={() => alert('sign button')} />
+      <FormButton buttonTitle='Signup' onPress={() => register(email, password)} />
     </View>
   );
 }
