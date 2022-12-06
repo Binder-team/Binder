32e53eaf66c4b3a806331fc11fabc2a0698f8081
Azu <azuping@gmail.com>
2022-12-07 00:53:57 +0900

    Fix : A number of bugs relating to bad file placement and packages

---
+++ components/Swipe.tsx
@@ -0,0 +1,101 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, StatusBar, Animated } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+
+//Fake data 
+const profiles = [
+  {
+    name:"John Doe",
+    age:27,
+    likes:["Hockey","Hiking"],
+    pic:"https://www.exampleimagelink.png"
+  },
+  {
+    name:"Alexis Texas",
+    age:22,
+    likes:["Parties","Bananas"],
+    pic:"https://www.exampleimagelink2.png"
+  },
+  {
+    name:"Jane Smith",
+    age:35,
+    likes:["Netflix","Wine"],
+    pic:"https://www.exampleimagelink3.png"
+  }
+]
+    //===IMPORTANT===
+    let index = 1;  //index should be declared outside of App to avoid duplicates.  
+    //It's here for now and resets every time this loads
+export default function Swipe(props: any) {
+
+  const [profile,setProfile] = useState(profiles[0]);
+  const translateX = new Animated.Value(0);
+
+  
+  const handleSwipe=(nativeEvent:any) =>{ // "nativeEvent" is like "e" but for gesture handler
+    console.log(nativeEvent);
+    //Currently leaving is as "Any"
+    
+    //swiping right
+    if(nativeEvent.translationX < -225){  //0,0 is bottom left
+      console.log("Swiped Right");
+      //Here we would add the code to save the user profile + book into our match list.
+      
+      index++;
+      setProfile(profiles[index%3]);
+    }
+    //swiping left
+    else if(nativeEvent.translationX > 225){
+      console.log("Swiped Left");
+
+      index++;
+      setProfile(profiles[index%3]);
+    }
+  };
+
+  const handlePan= Animated.event(
+    [{nativeEvent:{translationX:translateX}}],{useNativeDriver:true}
+  )  
+
+  return (
+    <View style={styles.container}>
+      <PanGestureHandler onHandlerStateChange={handleSwipe} onGestureEvent={handlePan} >
+        <Animated.View style={{backgroundColor:"yellow", width:"75%", height:"75%"}}>
+          <Image ></Image>
+          <Text>{profile.name}</Text>
+          <Text>Age: {profile.age}</Text>
+          <Text>Likes: {profile.likes.join(', ')}</Text>
+        </Animated.View>
+      </PanGestureHandler>
+      <StatusBar />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin:10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardContainer:{
+    flex:1,
+    width:"100%",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  card:{
+    backgroundColor: "rgb(230,230,230)",
+    width:"100%",
+    height:"100%",
+    borderRadius: 5,
+    position:'absolute',
+    borderWidth:1.5,
+    borderColor:'black',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
--- screens/BookMatchingScreen.tsx
+++ screens/BookMatchingScreen.tsx
@@ -1,16 +1,17 @@
 import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import Swipe from '../components/Swipe';
 import { RootTabScreenProps } from '../types';
-import Swipe from '../components/Swiping';
+
 
 //this is the main page where user swipes on a book or not. for now, just two buttons
 export default function BookMatchingScreen({ navigation }: RootTabScreenProps<'FindBookTab'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Find a book</Text>
+      {/* <Text style={styles.title}>Find a book</Text> */}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Swipe/>
+      <Swipe />
       <EditScreenInfo path="/screens/BookMatchingScreen.tsx" />
     </View>
   );
