fa33b42798e252ec74a7ed070bfebfe5bcc9d49a
Kudo Sanju <Noelle07072020@gmail.com>
2022-12-11 03:23:30 +0900

    feat: add scroll, separator, horizontal title display with flatlist

--- components/MyBooks.tsx
+++ components/MyBooks.tsx
@@ -1,63 +1,159 @@
-import React, { useEffect, useState } from "react";
-import { Text, StyleSheet, View, FlatList, Image } from "react-native";
+import React, {useEffect, useState} from 'react';
+import {
+  StatusBar,
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  SafeAreaView,
+  Image,
+  ScrollView,
+} from 'react-native';
+
 import axios from 'axios';
-import BookItem  from '../components/BookItem';
-import { Book } from '../types';
+
+import {Book} from '../types';
+import {Title} from 'react-native-paper';
 
 export type Props = {
   book: Book;
   BookItem: Function;
-}
+};
 
-const MyBooks: React.FC<Props> =({ book, BookItem }) => {
-    const [data, setData] = useState<[]>([]); //where all user's books get stored, as an array
-  
+const MyBooks: React.FC<Props> = ({book, BookItem}) => {
+  const [data, setData] = useState<[]>([]); //where all user's books get stored, as an array
+
   //axios get all books from user using 'https://binderapp-server.herokuapp.com/api/user_books'
-  //it's going to be stored in the state, data, then 
+  //it's going to be stored in the state, data, then
   //in the return statement, map through array to render
 
-  const handleFetch = async() => {
-    const res = await axios.get(`https://binderapp-server.herokuapp.com/api/user_books`);
+  const handleFetch = async () => {
+    const res = await axios.get(
+      `https://binderapp-server.herokuapp.com/api/user_books`,
+    );
     const data = await res.data;
     setData(data);
-    // console.log(data);
+    console.log(data);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     handleFetch();
-},[]);
-//map over data
-const showBooks = data.map((obj) => {
-    return (
-        
-        <View key={obj["id"]}>
-            <View>
-                <Image source={{uri:obj["thumbnail_url"],width: 50, height: 50,}}/>
-            </View>
+  }, []);
+  //map over data
+  // const showBooks = data.map(obj => {
+  //   return (
+  //     <View key={obj['id']}>
+  //       <View>
+  //         <Image source={{uri: obj['thumbnail_url'], width: 50, height: 50}} />
+  //       </View>
 
-            <Text>Title:{obj["title"]}</Text>
-            <Text>Author:{obj["author"]}</Text>
-            <Text>Condition:{obj["condition"]}</Text>
-            <Text></Text>
-        </View>
-    )
-})
-    return (
-       <View>
-        {showBooks}
-       </View> 
- 
+  //       <Text>Title:{obj['title']}</Text>
+  //       <Text>Author:{obj['author']}</Text>
+  //       <Text>Condition:{obj['condition']}</Text>
+  //     </View>
+  //   );
+  // });
+  //console.log(showBooks);
+
+  const oneBook = ({item}) => (
+    <View style={styles.item}>
+      <View style={styles.avatarContainer}>
+        <Image
+          style={{
+            // borderColor: 'blue',
+            // borderWidth: 2,
+            borderRadius: 3,
+            height: 80,
+            width: 80,
+          }}
+          source={{
+            uri: item.thumbnail_url,
+            width: 50,
+            height: 50,
+          }}
+        />
+      </View>
+      <View>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text>{item.author}</Text>
+        <Text>{item.condition}</Text>
+      </View>
+    </View>
+  );
+  const itemSeparator = () => {
+    return <View style={styles.separator} />;
+  };
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        //horizontal
+        ListFooterComponent={<View />}
+        ListFooterComponentStyle={{height: 200}}
+        //contentContainerStyle={{paddingBottom: 30}}
+        persistentScrollbar
+        //contentContainerStyle={{marginBottom: 20}}
+        data={data}
+        renderItem={oneBook}
+        ItemSeparatorComponent={itemSeparator}
+        showsHorizontalScrollIndicator={false}
+      />
+    </SafeAreaView>
   );
 };
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: StatusBar.currentHeight,
+//   },
+//   scrollView: {
+//     backgroundColor: '#fff',
+//     marginHorizontal: 30,
+//   },
+//   text: {
+//     fontSize: 20,
+//   },
+// });
+
 const styles = StyleSheet.create({
-  baseText: {
-    fontFamily: "Cochin"
+  // contentContainer: {
+  //   //width: '100%',
+  //   //flex: 1,
+  //   // backgroundColor: '#121212',
+  //   padding: 10,
+  //   //paddingVertical: 20,
+  // },
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#CCC',
   },
-  titleText: {
-    fontSize: 20,
-    fontWeight: "bold"
-  }
+  avatarContainer: {
+    backgroundColor: 'D9D9D9',
+    borderRadius: 100,
+    height: 89,
+    width: 89,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  item: {
+    //flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVetrical: 13,
+  },
+
+  avatar: {
+    height: 55,
+    width: 55,
+  },
+
+  // title: {
+  //   fontWeight: 600,
+
+  //   marginLeft: 13,
+  // },
 });
 
 export default MyBooks;
