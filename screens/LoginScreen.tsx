7790019720be02b56c7707065c28497ffff08e83
NicoletaBararu <nicoletabararu12@gmail.com>
2022-12-20 14:15:42 +0900

    feat: change widowSoftInputMode to adjustPan

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,24 +1,27 @@
-import { useState, useRef, useEffect, ChangeEvent } from 'react';
-import { StyleSheet, TouchableOpacity, Image, KeyboardAvoidingView} from 'react-native';
+import { StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { setToken, getToken, getUsername, setUsername, setPassword, getPassword } from '../components/userTokenManager';
-import axios from 'axios';
-import Navigation from '../navigation';
 import useAuth from '../hooks/useAuth';
 import { RootStackScreenProps } from '../types';
 import ButtonForm from '../components/ButtonForm';
 import FormInput from '../components/FormInput';
-
-
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 
 export default function LoginScreen({ navigation }: RootStackScreenProps<'CreateAccount'>) {
   const { signIn } = useAuth();
-  
-  return (
-    <View style={styles.container}>
-           <Image
-           style={styles.logo}
+
+
+  return (  
+    <KeyboardAwareScrollView style={styles.scroll}
+       enableOnAndroid={true}
+       extraHeight={0}
+       enableResetScrollToCoords={false}
+       bounces={false}
+       keyboardOpeningTime={0}
+       >
+   < View style={styles.container}>  
+      <Image style={styles.logo}
            resizeMode={'contain'}
            source={require('../assets/images/logo-transparent.png')}
            />
@@ -31,7 +34,7 @@ export default function LoginScreen({ navigation }: RootStackScreenProps<'Create
         autoCapitalize='none'
         autoCorrect={false}
        />
-         <FormInput
+      <FormInput
            // value={password}
             placeholderText="Enter Password"
              secureTextEntry={true}
@@ -39,26 +42,30 @@ export default function LoginScreen({ navigation }: RootStackScreenProps<'Create
               setPassword(e.nativeEvent.text);
             }}
             />
-             <TouchableOpacity>
-        <ButtonForm title='Sign In' onPress={signIn}/>
-    </TouchableOpacity>
-    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    <View style={styles.signUp}>
-    <Text>New user? Join here!</Text>
-    <TouchableOpacity >
-        <ButtonForm title='Create an Account' onPress={()=>{
-          navigation.navigate('CreateAccount')
-        }}/>
-    </TouchableOpacity>
-    </View>
-    </View> 
-   
+        <TouchableOpacity>
+          <ButtonForm title='Sign In' onPress={signIn}/>
+       </TouchableOpacity>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+           <View style={styles.signUp}>
+            <Text>New user? Join here!</Text>
+        <TouchableOpacity >
+            <ButtonForm title='Create an Account' onPress={()=>{
+                  navigation.navigate('CreateAccount')
+                  }}/>
+        </TouchableOpacity>
+      </View>
+     </View>
+  </KeyboardAwareScrollView>
+    
+    
   )
 }
  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width:'100%',
+    height:'100%',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor:'#FBF0DF',
@@ -68,24 +75,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
    logo: {
-    flex: 1,
-    alignSelf: 'center',
-    width:'70%',
-   // height: 10,
-   // aspectRatio: 1,
+    width:250,
+    height:200,
+   
     resizeMode: 'contain',  
    },
   separator: {
     marginVertical: 15,
     height: 3,
     width: '80%',
-    //marginBottom: 200,
   },
   signUp: {
-    justifyContent: 'center',
-    alignItems:'center',
-    marginBottom:200,
-    backgroundColor:'#FBF0DF'
+    backgroundColor:'#FBF0DF',
      
+  },
+  scroll: {
+    flex: 1,
+    backgroundColor: '#FBF0DF'
   }
 });
